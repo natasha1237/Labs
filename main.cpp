2df82326ccbbf26186352b4e2ae5b328072edbe8
natasha1237 <45146473+natasha1237@users.noreply.github.com>
2018-12-28 00:56:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,19 +5,15 @@
 
 using namespace std;
 
-int **create_arr(int a)
-{
+int **create_arr(int a) {
   int **arr = new int* [a];
 
-  for (int i = 0 ; i < a ; i++)
- {
+  for (int i = 0 ; i < a ; i++) {
     arr[i] = new int[a];
  }
 
-  for (int i = 0 ; i < a ; i++)
- {
-    for (int j = 0 ; j < a ; j++)
-    {
+  for (int i = 0 ; i < a ; i++) {
+    for (int j = 0 ; j < a ; j++) {
       arr[i][j] = rand()% 100 - 50;
       cout << arr [i][j] << "\t";
     }
@@ -26,18 +22,13 @@ int **create_arr(int a)
   return arr;
 }
 
-int diag_sum(int **arr, int a)
-{
+int diag_sum(int **arr, int a) {
   int sum = 0 ;
-  for (int i = 0 ; i < a ; i++)
-  {
-    for (int j = 0 ; j < a ; j++)
-    {
-      if (arr[i][0] < 0)
-      {
+  for (int i = 0 ; i < a ; i++) {
+    for (int j = 0 ; j < a ; j++) {
+      if (arr[i][0] < 0) {
         cout << arr[i][j] << "\t";
-        if (i==j || i+j+1==a)
-        {
+        if (i==j || i+j+1==a) {
           sum += abs(arr[i][j]);
         }
       }
@@ -49,17 +40,12 @@ int diag_sum(int **arr, int a)
   return sum;
 }
 
-int **sort_array(int **arr, int a)
-{
+int **sort_array(int **arr, int a) {
 int temp ;
- for(int i = 0; i<a; i++)
-{
-    for(int j=0; j<a; j++)
-    {
-        for(int k=j+1; k<a; k++)
-        {
-          if(arr[i][k] < arr[i][j])
-            {
+ for(int i = 0; i<a; i++) {
+    for(int j=0; j<a; j++) {
+        for(int k=j+1; k<a; k++) {
+          if(arr[i][k] < arr[i][j]) {
               temp = arr[i][j];
               arr[i][j] = arr[i][k];
               arr[i][k] = temp;
@@ -68,18 +54,15 @@ int temp ;
     }
  }
 cout << "Your sorted array : \n\n";
-for (int i = 0 ; i < a ; i++)
- {
-    for (int j = 0 ; j < a ; j++)
-    {
+for (int i = 0 ; i < a ; i++) {
+    for (int j = 0 ; j < a ; j++) {
       cout << arr[i][j] << "\t";
     }
     cout << endl;
   }
 }
 
-int main()
-{
+int main() {
   srand(time(NULL));
   int a ;
   cout << "Enter a:";
@@ -99,29 +82,25 @@ int main()
   cout << endl;
 
   int sum = sum1;
-  if (sum1 > sum2)
-  {
+  if (sum1 > sum2) {
     sum = sum2;
-    if (sum2 > sum3)
-    {
+    if (sum2 > sum3) {
       sum = sum3;
        sort_array(arr3,a);
     }
-    else
-    {
+    else {
       sort_array(arr2,a);
     }
   }
-  else if (sum1 > sum3)
-  {
+  else if (sum1 > sum3) {
     sum = sum3;
     sort_array(arr3,a);
   }
-  else
-  {
+  else {
     sort_array(arr1,a);
   }
 
 cout << " min sum = " << sum;
 
 }
+
