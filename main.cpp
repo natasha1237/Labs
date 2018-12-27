b4aa70dfe3d3621be267610bb0283780e6eaa9be
natasha1237 <45146473+natasha1237@users.noreply.github.com>
2018-12-27 21:17:02 +0200

    Lab #9

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int *getCharCountArray(char *str)
+{
+   int *count = (int *)calloc(sizeof(int), 256);
+   int i;
+   for (i = 0; *(str+i);  i++)
+      count[*(str+i)]++;                
+   return count;
+}
+
+
+void NonRepeating(char *str)
+{
+  int *count = getCharCountArray(str);
+  int i = 0,k = 0;
+  int index = -1;
+    for (i = 0; *(str+i);  i++)
+      {
+        if (count[*(str+i)] == 1)
+        {
+          k++;
+          index = i;
+          printf("%i non-repeating character is %c and it has %i index\n\n", k,str[index],index);
+        }
+      }
+
+  free(count);
+    if (index == -1)
+    printf("Either all characters are repeating or string is empty");
+}
+using namespace std;
+
+int main()
+{
+  char str[256];
+  cout << "Enter string : " ;
+  cin >> str ;
+  NonRepeating(str);
+  getchar();
+  return 0;
+}
